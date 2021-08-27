c3c40f91fde9907fc371d5c137bff26636d617aa
Oskar Nummedal <oskarnummedal@gmail.com>
2021-08-27 11:41:33 +0100

    Adding force argument to unit tests

--- tests/testthat/test_validate.R
+++ tests/testthat/test_validate.R
@@ -17,15 +17,15 @@ hyperparameters_tree = list(dependent_variable = 'response',
                             print_safety_check=20)
 
 test_that("Supervised without response variable", {
-  expect_error(citrus::validate(output_preprocess, hyperparameters = hyperparameters_tree), regexp = "Columns missing: response")
+  expect_error(citrus::validate(output_preprocess, force = TRUE, hyperparameters = hyperparameters_tree), regexp = "Columns missing: response")
 })
 
 test_that("Correct error when customerid is missing.", {
-  expect_error(citrus::validate(preprocess_no_customerid, hyperparameters = hyperparameters_tree), regexp = "Columns missing: customerid")
+  expect_error(citrus::validate(preprocess_no_customerid, force = TRUE, hyperparameters = hyperparameters_tree), regexp = "Columns missing: customerid")
 })
 
 test_that("Throw error when too many categorical levels", {
-  expect_error(citrus::validate(preprocess_too_many_categories, hyperparameters = hyperparameters_tree), regexp = "Categorical Columns have too many levels: faulty_feature")
+  expect_error(citrus::validate(preprocess_too_many_categories, force = FALSE, hyperparameters = hyperparameters_tree), regexp = "Categorical Columns have too many levels: faulty_feature")
 })
 
 test_that("Unique customer count", {
